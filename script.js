7005dde53ac946063c5f12b7512889cab24e5ec2
Praveen <109732844+Praveen5512@users.noreply.github.com>
2024-10-12 11:12:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+    var typed = new Typed('#typed', {
+        strings: [' Praveen.....'],
+        typeSpeed: 100,
+        backSpeed: 25,
+        loop: true,
+        showCursor: false,
+        onComplete: function() {
+            // new Typed('#typed-role', {
+            //     strings: ['Aspiring Full Stack Developer'],
+            //     typeSpeed: 100,
+            //     backSpeed: 50,
+            //     loop: false,
+            //     showCursor: false
+            // });
+        }
+    });
+    AOS.init();
+
+
+    
